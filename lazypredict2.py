65eeb08ab790f67572a1c855f08970aa372cc03b
Amr Ez Eldin Elsayed Rashed <amrrashed200@gmail.com>
2022-11-13 22:25:31 +0300

    Update lazypredict2.py

--- lazypredict2.py
+++ lazypredict2.py
@@ -1,18 +1,9 @@
 #link https://towardsdatascience.com/how-to-run-40-regression-models-with-a-few-lines-of-code-5a24186de7d
 #https://towardsdatascience.com/how-to-run-30-machine-learning-models-with-2-lines-of-code-d0f94a537e52
 # Importing important libraries
-import numpy as np
 import pandas as pd  # To read data
 from lazypredict.Supervised import LazyRegressor
-from pandas.plotting import scatter_matrix
-# Scikit-learn packages
-from sklearn.linear_model import LinearRegression
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import ExtraTreesRegressor
-from sklearn import metrics
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import mean_squared_error
-from sklearn import preprocessing
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.preprocessing import StandardScaler
 # Hide warnings
@@ -34,7 +25,7 @@ X2 = ss.fit_transform(X1)
 
 y = data.iloc[:,7]# 4:8 FOR DB1
 # Call train_test_split on the data and capture the results
-X_train, X_test, y_train, y_test = train_test_split(X1, y, random_state=3,test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X2, y, random_state=3,test_size=0.2)
 reg = LazyRegressor(ignore_warnings=False, custom_metric=None)
 models, predictions = reg.fit(X_train, X_test, y_train, y_test)
 print(models)
